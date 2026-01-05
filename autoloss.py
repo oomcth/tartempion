0623ebd61380c82391f27d01934a936c38f9f46b
oomcth <mathis.scheffler@ens.psl.eu>
2026-01-05 16:42:04 +0100

    new strategy for learning

--- autoloss.py
+++ autoloss.py
@@ -24,6 +24,10 @@ class torch_SE3_loss(Function):
         grad_output: torch.Tensor,
     ):
         grad = np.array(ctx.SE3_loss_workspace.d_SE3_loss())
+        has_nan = np.isnan(grad).any()
+        has_large = np.abs(grad).max() > 1e-5
+        if has_nan or has_large:
+            raise
         return (
             None,
             torch.from_numpy(grad).to(grad_output.device).to(grad_output.dtype),
